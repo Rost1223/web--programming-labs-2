4c3cbe76e3abc64dfb9ee6259dc3d8f77a4eaa57
ростислав разуванов <rost12.basketball@yandex.ru>
2023-12-13 03:59:17 +0700

    Железнодорожный билет

--- lab3.py
+++ lab3.py
@@ -46,3 +46,58 @@ def pay():
 @lab3.route('/lab3/success')
 def success():
     return render_template('success.html')
+
+
+@lab3.route('/lab3/buy_ticket')
+def buy():
+    errors = {}
+    name = request.args.get('name')
+    type_ticket = request.args.get('type_ticket')
+    shelf = request.args.get('shelf')
+    baggage = request.args.get('baggage')
+    age = request.args.get('age')
+    exit_point = request.args.get('exit_point')
+    arrival_point = request.args.get('arrival_point')
+    date = request.args.get('date')
+
+    if name == '':
+        errors['name'] = 'Заполните поле!'
+
+
+    if age is None or age == '':
+        errors['age'] = 'Заполните поле!'
+    else:
+        age= int(age)
+        if age < 18 or age > 120:
+            errors['age'] = 'Возраст пассажира должен быть от 18 до 120 лет.'
+
+
+    if exit_point == '':
+        errors['exit_point'] = 'Заполните поле!'
+
+    if arrival_point == '':
+        errors['arrival_point'] = 'Заполните поле!'
+
+    if date == '':
+        errors['date'] = 'Заполните поле!'
+
+    if request.args.get('baggage') == 'on':
+        baggage = 'есть'
+    else:
+        baggage = 'нет'
+
+    if request.args.get('type_ticket') == 'kid':
+        type_ticket = 'детский'
+    else:
+        type_ticket = 'взрослый'
+
+    if request.args.get('shelf') == 'lower':
+        shelf = 'нижняя'
+    elif request.args.get('shelf') == 'upper':
+        shelf = 'верхняя'
+    elif request.args.get('shelf') == 'upper side':
+        shelf = 'верхняя боковая'
+    else:
+        shelf = 'нижняя боковая'
+
+    return render_template('buy_ticket.html', name=name, type_ticket=type_ticket, shelf=shelf, baggage=baggage, age=age, exit_point=exit_point, arrival_point=arrival_point, date=date, errors=errors)
